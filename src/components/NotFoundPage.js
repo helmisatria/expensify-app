951af4a3282b00abcdd2ee822cf12dec9439118c
helmisatria <satriahelmi@gmail.com>
2017-10-24 18:41:28 +0700

    conflict on params

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotfoundPage = () => (
+  <div>
+    Page not found - <Link to="/" href="/">Go home</Link>
+  </div>
+);
+
+export default NotfoundPage;
