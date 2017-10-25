51bd071fb0002155901667e997cd84ecc733437c
helmisatria <satriahelmi@gmail.com>
2017-10-25 09:03:52 +0700

    after react-router

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact>Home</NavLink>
     <NavLink to="/create" activeClassName="is-active" >Create</NavLink>
-    <NavLink to="/edit/asd" activeClassName="is-active" >Edit</NavLink>
     <NavLink to="/help" activeClassName="is-active" >Help</NavLink>
   </header>
 );
