2e573e54b7959e2ecbf04866f0c8cbee8712ef99
helmisatria <satriahelmi@gmail.com>
2017-11-04 01:03:05 +0700

    testing component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-import 'normalize.css/normalize.css';
+import { addExpense } from './actions/expenses';
+import { setTextFilter, sortByAmount } from './actions/filters';
+
+import getVisibleExpenses from './selectors/expenses';
+
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 300 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 3000 }));
+
+const state = store.getState();
+
+// console.log(store.getState());
+console.log(getVisibleExpenses(state.expenses, state.filters));
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
+
+const ExpenseListItem = ({
+  dispatch, description, amount, createdAt, id,
+}) => (
+  <div>
+    <Link
+      href={`/edit/${id}`}
+      to={`/edit/${id}`}
+    >
+      <h3>{description}</h3>
+    </Link>
+    <p>{amount} - {createdAt}</p>
+  </div>
+);
+
+export default connect()(ExpenseListItem);
